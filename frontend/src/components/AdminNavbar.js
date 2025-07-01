a600bff4858ab52c7ddbcebbe94d896f24e842da
Pese <146886140+Pesepvtltd@users.noreply.github.com>
2025-07-01 16:51:18 +0530

    update

--- frontend/src/components/AdminNavbar.js
+++ frontend/src/components/AdminNavbar.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 const AdminNavbar = () => {
   return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-dark px-4">
-      <a className="navbar-brand fw-bold text-white" href="#">Admin Panel</a>
+      <Link className="navbar-brand fw-bold text-white" to="/admin">Admin Panel</Link>
       <div className="collapse navbar-collapse">
         <ul className="navbar-nav ms-auto gap-3">
           <li className="nav-item">
